b17de2c5288cce2558be4a4a03a6eae8ec060148
username <wecp-dummy@gmail.com>
2024-12-26 08:04:10 +0000

    Day-1 task

--- backend/src/main/java/com/wecp/progressive/config/DatabaseConnectionManager.java
+++ backend/src/main/java/com/wecp/progressive/config/DatabaseConnectionManager.java
@@ -1,6 +1,33 @@
 package com.wecp.progressive.config;
 
-public class DatabaseConnectionManager {
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
-    
+public class DatabaseConnectionManager {
+    private static final Properties properties= new Properties();
+    static{
+        loadProperties();
+    }
+
+    private static void loadProperties(){
+        try(InputStream input=DatabaseConnectionManager.class.getClassLoader().getResourceAsStream("application.properties")){
+            if(input==null){
+                throw new IllegalStateException("resource.properties not fpund in classpath");
+            }
+            properties.load(input);
+        }catch(IOException e){
+            throw new RuntimeException("Error loading properties file",e);
+        }
+    }
+    public static Connection getConnection() throws SQLException{
+        String url = properties.getProperty("spring.datasource.url");
+        String user = properties.getProperty("spring.datasource.username");
+        String password = properties.getProperty("Spring.datasource.password");
+        return DriverManager.getConnection(url,user,password);
+    }
+
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJdbc.java
@@ -1,5 +1,35 @@
 package com.wecp.progressive.service.impl;
 
-public class MatchServiceImplJdbc  {
+import java.util.List;
+
+import com.wecp.progressive.entity.Match;
+import com.wecp.progressive.service.MatchService;
+
+public class MatchServiceImplJdbc  implements MatchService{
+
+    @Override
+    public List<Match> getAllMatches() {
+        return List.of();
+    }
+
+    @Override
+    public Match getMatchById(int matchId) {
+        return null;
+    }
+
+    @Override
+    public Integer addMatch(Match match) {
+        return -1;
+    }
+
+    @Override
+    public void updateMatch(Match match) {
+        
+    }
+
+    @Override
+    public void deleteMatch(int matchId) {
+        
+    }
 
 }
